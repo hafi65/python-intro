42e1ae3588b77bf90995a6dd08b9dbc9ff7f6b7b
hafi65 <hafizalabiaamaan@gmail.com>
2024-12-29 21:10:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello world  my name is laiba. ")
+print("I am a python devloper.")
+print("Addition of two number " ,109 + 3464)
+print("multiplication of two number", 45* 465)
+print(55745-76565)
+print("print all keywords")
+import keyword
+print(keyword.kwlist)
